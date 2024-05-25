3b0b97b0af69cf01fd4baf551e4abc95c624161d
mar1n <30738216+mar1n@users.noreply.github.com>
2024-05-25 15:41:04 +0100

    render props  lifting state

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './patterns/rednerPropsLiftingState';
 import reportWebVitals from './reportWebVitals';
 import RenderProps from './patterns/renderPropsPattern';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 const Title = (props) => props.render();
 RenderProps()
-// root.render(
-//   <React.StrictMode>
-//     <RenderProps/>
-//   </React.StrictMode>
-// );
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
+);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
